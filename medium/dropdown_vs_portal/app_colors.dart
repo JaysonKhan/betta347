867c17b474b3588b07a775a42badd91d64f288be
Jahongir Qo'ziboyev <67469502+betta347@users.noreply.github.com>
2023-10-29 13:40:50 +0500

    Create app_colors.dart

---
+++ medium/dropdown_vs_portal/app_colors.dart
@@ -0,0 +1,25 @@
+import 'dart:ui';
+
+class AppColors{
+
+  static Color lightGreen = const Color(0xFF00A795);
+  static Color darkGreen = const Color(0xFF008F7F);
+  static Color dark = const Color(0xFF2A2A2D);
+  static Color darker = const Color(0xFF1E1E20);
+  static Color darkest = const Color(0xFF141416);
+  static Color white = const Color(0xFFE7E7E7);
+
+  static Color whiteDA = const Color(0xFFDADADA);
+  static Color whiteD1 = const Color(0xFFD1D1D1);
+  static Color whiteE6 = const Color(0xFFE6E6E6);
+  static Color greyF1 = const Color(0xFFF1F1F1);
+  static Color grey = const Color(0xFF999999);
+  static Color greyA6 = const Color(0x33A6A6A6);
+  static Color grey2A = const Color(0xFF2A2A2A);
+  static Color grey4D = const Color(0xFF4D4D4D);
+
+  static Color green = const Color(0xFF49B7A5);
+  static Color yellow = const Color(0xFFFDAB2A);
+  static Color red = const Color(0xFFFF426D);
+
+}
